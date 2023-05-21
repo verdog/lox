c0227e68f928a8ac7d7d7bd22aa3eeeacf326ffc
Josh <j5d1c7@gmail.com>
2023-05-21 17:31:25 -0500

    use refAllDecls to run tests

--- treewalk/src/main.zig
+++ treewalk/src/main.zig
@@ -2,6 +2,8 @@ pub fn main() !void {
     std.debug.print("jlox", .{});
 }
 
-test "run all tests" {}
+test "run all tests" {
+    std.testing.refAllDecls(@This());
+}
 
 const std = @import("std");
